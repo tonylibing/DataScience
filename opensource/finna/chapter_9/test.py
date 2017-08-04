8bcc1326216a3c1c3131e40d8b897a8945d1e98d
tangning593 <tangning593@lu.com>
2017-08-04 09:52:18 +0800

    add bayes opt

--- opensource/finna/chapter_9/test.py
+++ opensource/finna/chapter_9/test.py
@@ -10,6 +10,8 @@ from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import *
+from sklearn.grid_search import GridSearchCV
+from bayes_opt import BayesianOptimization
 import statsmodels.api as sm
 import xgboost as xgb
 import sys
@@ -328,3 +330,32 @@ print 'gbm training:', auc, accuracy, cm
 
 # gbm training: 0.907013605027 0.782416666667 [[8578 2497]
 #  [ 114  811]]
+
+def xgbcv(max_depth,n_estimators,learning_rate,subsample,colsample_bytree,min_child_weight,gamma):
+    gbm = xgb.XGBClassifier(max_depth=int(max_depth), n_estimators=int(n_estimators), learning_rate=learning_rate,
+                            subsample=subsample, colsample_bytree=colsample_bytree, 
+                            min_child_weight = min_child_weight, gamma = gamma, 
+                            objective="binary:logistic", seed=999,nthread=7)
+    gbm.fit(X,y)
+    pred_y = gbm.predict(X_test)
+    pred_score_y = gbm.predict_proba(X_test)[:, 1]
+    auc = roc_auc_score(y_test, pred_score_y)
+    with open('xgb_bayes_opt_results.txt','a') as f: f.write("max_depth:%f,n_estimators:%f,learning_rate:%f,subsample:%f,colsample_bytree:%f,min_child_weight:%f,gamma:%f,auc:%f\n"%(max_depth,n_estimators,learning_rate,subsample,colsample_bytree,min_child_weight,gamma,auc))
+    return auc
+
+xgbBO = BayesianOptimization(xgbcv,
+    {
+    'max_depth':(int(10),int(12)),
+    'n_estimators':(int(20),int(100)),
+    'learning_rate':(0.05,0.1),
+    'subsample':(0.3,0.5),
+    'colsample_bytree':(0.6,0.8),
+    'min_child_weight':(1,40),
+    'gamma':(0.05,1)
+    })
+
+xgbBO.maximize(niter=20)
+print('-'*53)
+print('Final Result')
+print('xgboost:%f' % xgbBO.res['max']['max_val'])
+print('xgboost:%s' % xgbBO.res['max']['max_params'])
