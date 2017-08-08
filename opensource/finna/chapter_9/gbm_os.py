f9357a01449456b773534f10fbcceaf61f4e5892
ataosky <ataosky@gmail.com>
2017-08-08 23:18:38 +0800

    update

--- opensource/finna/chapter_9/gbm_os.py
+++ opensource/finna/chapter_9/gbm_os.py
@@ -39,7 +39,7 @@ y = trainData['target']
 y = np.array(y)
 
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=2017,stratify=y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=2017,stratify=y)
 X_train.shape, y_train.shape
 
 oversampler=SMOTE(random_state=2017)
