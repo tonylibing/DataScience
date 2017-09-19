31a63ccecc2704894a9dbdf29509915b61be67ae
ataosky <ataosky@gmail.com>
2017-09-19 13:27:08 +0800

    update

--- cvr/TencentSocialAds/cvr.py
+++ cvr/TencentSocialAds/cvr.py
@@ -17,101 +17,128 @@ import processor
 reload(processor)
 from processor import *
 
-class LogInfoFeature(TransformerMixin):
-    #Idx,logTimes,logDays,logDayGap,logGapAVG,mostCountCode,leastCountCode,mostCountCate,leastCountCate,codeEntropy,cateEntropy,logDuration
-    def transform(self, df):
-        df['logInfo'] = df['LogInfo3'].map(lambda x: datetime.datetime.strptime(x, '%Y-%m-%d'))
-        df['Listinginfo'] = df['Listinginfo1'].map(lambda x: datetime.datetime.strptime(x, '%Y-%m-%d'))
-        df['ListingGap'] = df[['logInfo', 'Listinginfo']].apply(lambda x: (x[1] - x[0]).days, axis=1)
-        df['LogAction'] = df[['LogInfo1','LogInfo2']].apply(lambda x:'_'.join(x),axis=1)
-        action_cnt = df.groupby(['Idx','LogAction'])['LogInfo3'].count().unstack('LogAction').fillna(0)
-        action_cnt.columns = ['action_cnt_' + x  for x in action_cnt.columns.values.tolist()]
-        action_cnt.reset_index(inplace=True)
 
-        LogTimes = df.groupby(['Idx'])['LogInfo3'].count()
-        LogTimes.columns=['logTimes']
-        LogTimes.reset_index(inplace=True)
+train = pd.read_csv('train.csv')
+test = pd.read_csv('test.csv')
+ad = pd.read_csv('ad.csv')
+app = pd.read_csv('app_categories.csv')
+user = pd.read_csv('user.csv')
 
-        LogDays = df.groupby(['Idx'])['LogInfo3'].nunique()
-        LogDays.columns=['logDays']
-        LogDays.reset_index(inplace=True)
+def get_time_day(t): #click time is DDHHMM
+    t = str(t)
+    t=int(t[0:2])
+    return t
 
-        logDayGap =  df.groupby(['Idx'])['LogInfo3'].max()
+def get_time_hour(t):
+    t = str(t)
+    t=int(t[2:4])
+    if t<6:
+        return 0
+    elif t<12:
+        return 1
+    elif t<18:
+        return 2
+    else:
+        return 3
 
+train['clickTime_day'] = train['clickTime'].apply(get_time_day)
+train['clickTime_hour']= train['clickTime'].apply(get_time_hour)
 
-        return df
+test['clickTime_day'] = test['clickTime'].apply(get_time_day)
+test['clickTime_hour']= test['clickTime'].apply(get_time_hour)
 
-    def fit(self, *_):
-        return self
+# method 1
+def categories_first_class(cate):
+    cate = str(cate)
+    if len(cate)==1:
+        if int(cate)==0:
+            return 0
+    else:
+        return int(cate[0])
 
-class UpdateInfoFeature(TransformerMixin):
-    #Idx,ListingInfo1,UserupdateInfo1,UserupdateInfo2
-    #Idx, updateTimes, updateDays, updateDayGap, updateGapAVG, mostCountInfo, leastCountInfo, infoEntropy, infoUpdateDuration
-    def transform(self, df):
-        df['ListingInfo'] = df['ListingInfo1'].map(lambda x: datetime.datetime.strptime(x, '%Y/%m/%d'))
-        df['UserupdateInfo'] = df['UserupdateInfo2'].map(lambda x: datetime.datetime.strptime(x, '%Y/%m/%d'))
-        data3GroupbyIdx = pd.DataFrame({'Idx': df['Idx'].drop_duplicates()})
-        # items = set(df['UserupdateInfo1'].values)
-        time_window = [7, 30, 60, 90, 120, 150, 180]
-        for tw in time_window:
-            df['TruncatedLogInfo'] = df['ListingInfo'].map(lambda x: x + datetime.timedelta(-tw))
-            temp = df.loc[df['UserupdateInfo'] >= df['TruncatedLogInfo']]
+# method 2
+def categories_second_class(cate):
+    cate = str(cate)
+    if len(cate)<3:
+        return 0
+    else:
+        return int(cate[1:])
 
-            # frequency of updating
-            freq_stats = temp.groupby(['Idx'])['UserupdateInfo1'].count().to_dict()
-            data3GroupbyIdx['UserupdateInfo_' + str(tw) + '_freq'] = data3GroupbyIdx['Idx'].map(
-                lambda x: freq_stats.get(x, 0))
+app["app_categories_first_class"] = app['appCategory'].apply(categories_first_class)
+app["app_categories_second_class"] = app['appCategory'].apply(categories_second_class)
 
-            # number of updated types
-            Idx_UserupdateInfo1 = temp[['Idx', 'UserupdateInfo1']].drop_duplicates()
-            uniq_stats = Idx_UserupdateInfo1.groupby(['Idx'])['UserupdateInfo1'].count().to_dict()
-            data3GroupbyIdx['UserupdateInfo_' + str(tw) + '_unique'] = data3GroupbyIdx['Idx'].map(
-                lambda x: uniq_stats.get(x, 0))
+### user
+user[user['age']!=0].describe() #age 0 means unknown
 
-            # average count of each type
-            data3GroupbyIdx['UserupdateInfo_' + str(tw) + '_avg_count'] = data3GroupbyIdx[
-                ['UserupdateInfo_' + str(tw) + '_freq', 'UserupdateInfo_' + str(tw) + '_unique']]. \
-                apply(lambda x: x[0] * 1.0 / x[1], axis=1)
+# deal with age
+def age_process(age):
+    age = int(age)
+    if age==0:
+        return 0
+    elif age<15:
+        return 1
+    elif age<21:
+        return 2
+    elif age<28:
+        return 3
+    else:
+        return 4
 
-            # Idx_UserupdateInfo1_V2 = Idx_UserupdateInfo1.assign(update_cnt = 1).set_index(["Idx", "UserupdateInfo1"]).unstack("UserupdateInfo1").fillna(0)
-            # Idx_UserupdateInfo1_V2.columns = [ x[0]+x[1]+str(tw) for x in Idx_UserupdateInfo1_V2.columns.values.tolist()]
-            # Idx_UserupdateInfo1_V2.reset_index(inplace=True)
-            Idx_UserupdateInfo1_V2 = temp.groupby(['Idx','UserupdateInfo1'])['UserupdateInfo2'].count().unstack('UserupdateInfo1').fillna(0)
-            Idx_UserupdateInfo1_V2.columns =  [ 'update_cnt'+x+'_'+str(tw) for x in Idx_UserupdateInfo1_V2.columns.values.tolist()]
-            Idx_UserupdateInfo1_V2.reset_index(inplace=True)
+# deal with provice
+def process_province(hometown):
+    hometown = str(hometown)
+    province = int(hometown[0:2])
+    return province
 
-            data3GroupbyIdx=pd.merge(data3GroupbyIdx, Idx_UserupdateInfo1_V2, on='Idx', how='left')
+#城市处理
+def process_city(hometown):
+    hometown = str(hometown)
+    if len(hometown)>1:
+        province = int(hometown[2:])
+    else:
+        province = 0
+    return province
 
-        return data3GroupbyIdx
-
-
-    def fit(self, *_):
-        return self
-
-# classification_model= reload(classification_model)
-path= 'D:/workspace/DataScience/data/ppd/'
-#path= '/home/tanglek/workspace/DataScience/data/ppd/'
-data1 = pd.read_csv(path+'Training Set/PPD_LogInfo_3_1_Training_Set.csv', header = 0)
-data2 = pd.read_csv(path+'Training Set/PPD_Training_Master_GBK_3_1_Training_Set.csv', header = 0,encoding = 'gbk')
-data3 = pd.read_csv(path+'Training Set/PPD_Userupdate_Info_3_1_Training_Set.csv', header = 0)
-
-def process_data(df):
-    data1_pip = Pipeline([('drop_missing',DropColumnTransformer()),
-                         ('drop_row',RowMissingDroperTransformer())])
-
-    data2_pip = Pipeline([('lower_string', StripTransformer('UserInfo_9'))])
-
-    data3_pip = Pipeline([('lower_string',LowerTransformer('UserupdateInfo1')),
-                          ('strip_city',StripTransformer(['UserInfo_9'],'市'))])
-
-    d1=data1_pip.fit(data1)
-    d2 = data2_pip.fit(data2)
-    d3=data3_pip.fit(data3)
+user['age_process'] = user['age'].apply(age_process)
+user["hometown_province"] = user['hometown'].apply(process_province)
+user["hometown_city"] = user['hometown'].apply(process_city)
+user["residence_province"] = user['residence'].apply(process_province)
+user["residence_city"] = user['residence'].apply(process_city)
 
 
+# merge data
+train_user = pd.merge(train,user,on='userID')
+train_user_ad = pd.merge(train_user,ad,on='creativeID')
+train_user_ad_app = pd.merge(train_user_ad,app,on='appID')
+train_user_ad_app.head()
+
+test_user = pd.merge(test,user,on='userID')
+test_user_ad = pd.merge(test_user,ad,on='creativeID')
+test_user_ad_app = pd.merge(test_user_ad,app,on='appID')
+test_user_ad_app.head()
 
 
+feature_columns = ['creativeID','userID','positionID',
+ 'connectionType','telecomsOperator','clickTime_day','clickTime_hour','age', 'gender' ,'education',
+ 'marriageStatus' ,'haveBaby' , 'residence' ,
+ 'hometown_province', 'hometown_city','residence_province', 'residence_city',
+ 'adID', 'camgaignID', 'advertiserID', 'appID' ,'appPlatform' ,
+ 'app_categories_first_class' ,'app_categories_second_class']
 
+train_x = train_user_ad_app[feature_columns]
+test_x = test_user_ad_app[feature_columns]
+
+train_y = pd.DataFrame(train_user_ad_app['label'])
+
+# save as csv
+train_x.to_csv('train_x.csv',index=False)
+train_y.to_csv('train_y.csv',index=False)
+test_x.to_csv('test_x.csv',index=False)
+
+
+#feature preprocessing
+#for linear models:ID level features(one-hot,feature hashing),continuous features(binning),categorical features(ordinal,one-hot)
+#for non-linear models:ID level features(one-hot),raw continuous features,categorical features
 #features with date, category manipulation without feature importance
 def get_basic_features(df, ordinal, categorical,date_manip ,cont):
     date_pip = Pipeline([('extract',column_extractor.column_extractor(date_manip)),
@@ -141,40 +168,6 @@ def get_basic_features(df, ordinal, categorical,date_manip ,cont):
                                                     ('continuous',continuous),
                                                     ('ordinal_pip',ordinal_pip),
                                                     ('one_hot',one_hot)])),
-                        ('cleanup',cleanup_transformer.cleanup_transformer())])
-
-    return features, features.transform(df)
-
-#features with date, category manipulation without feature importance
-def get_poly_features(df, ordinal, categorical,date_manip ,cont):
-    date_pip = Pipeline([('extract',column_extractor.column_extractor(date_manip)),
-                ('date_manip', date_transformer.date_transformer('%Y-%m-%d')),
-                ('d-m-y-q-dow', FeatureUnion([('day',date_transformer.day_of_month_transformer()),
-                                            ('month',date_transformer.month_transformer()),
-                                            ('dow',date_transformer.day_of_week_transformer()),
-                                            ('quarter',date_transformer.month_quarter_transformer()),
-                                            ('year',date_transformer.year_transformer())])),
-                ('impute',imputing_transformer.imputing_transformer(Imputer(strategy='most_frequent')))])
-
-    continuous = Pipeline([
-            ('extract', column_extractor.column_extractor(cont)),
-            ('impute',imputing_transformer.imputing_transformer(Imputer(strategy='most_frequent'))),
-            ('scale', Normalizer())])
-
-    ordinal_pip = Pipeline([('extract', column_extractor.column_extractor(ordinal)),
-                            ('ord', categorical_transformer.ordinal_transformer(ordinal)),
-                           ('impute',imputing_transformer.imputing_transformer(Imputer(strategy='most_frequent')))])
-
-    one_hot = Pipeline([('extract', column_extractor.column_extractor(categorical)),
-                        ('lab_enc', categorical_transformer.label_transformer()),
-                        ('one_hot', ModelTransformer.ModelTransformer(OneHotEncoder(sparse=False)))])
-
-
-    features = Pipeline([('parallel', FeatureUnion([('date',date_pip),
-                                                    ('continuous',continuous),
-                                                    ('ordinal_pip',ordinal_pip),
-                                                    ('one_hot',one_hot)])),
-                        ('poly',ModelTransformer.ModelTransformer(PolynomialFeatures(degree=2))),
                         ('cleanup',cleanup_transformer.cleanup_transformer())])
 
     return features, features.transform(df)
