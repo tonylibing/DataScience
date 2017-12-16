3e48856e120df425b30532616971f8b23c5952ed
tanglek <ataosky@gmail.com>
2017-12-16 15:07:46 +0800

    add only gbdt features option

--- feature/processor.py
+++ feature/processor.py
@@ -903,7 +903,8 @@ class ReduceVIF(BaseEstimator, TransformerMixin):
 
 
 class XgboostLRClassifier(BaseEstimator):
-    def __init__(self,n_estimators=30,learning_rate =0.3,max_depth=3,min_child_weight=1,gamma=0.3,subsample=0.7,colsample_bytree=0.7,objective= 'binary:logistic',nthread=-1,scale_pos_weight=1,reg_alpha=1e-05,reg_lambda=1,seed=27,lr_penalty='l2', lr_c=1.0, lr_random_state=42):
+    def __init__(self,combine_feature = False,n_estimators=30,learning_rate =0.3,max_depth=3,min_child_weight=1,gamma=0.3,subsample=0.7,colsample_bytree=0.7,objective= 'binary:logistic',nthread=-1,scale_pos_weight=1,reg_alpha=1e-05,reg_lambda=1,seed=27,lr_penalty='l2', lr_c=1.0, lr_random_state=42):
+        self.combine_feature = combine_feature
         #gbdt model parameters
         self.n_estimators=n_estimators
         self.learning_rate=learning_rate
@@ -955,105 +956,108 @@ class XgboostLRClassifier(BaseEstimator):
             num_leaves = np.amax(pred_leaves)
 
         gbdt_feature_matrix = self.one_hot_encoder.fit_transform(pred_leaves)
-        print("orginfeatures:{0},predleaves:{1}".format(origin_features.shape,gbdt_feature_matrix.shape))
+        print("orgin_features:{0},pred_leaves:{1}".format(origin_features.shape,gbdt_feature_matrix.shape))
         gbdt_lr_feature_matrix = np.concatenate((origin_features,gbdt_feature_matrix.todense()),axis=1)
         return gbdt_lr_feature_matrix
 
-
     ##切割训练
-    def fit_model_split(self,X_train,y_train,X_test,y_test):
+    def fit_model_split(self, X_train, y_train, X_test, y_test):
         ##X_train_1用于生成模型  X_train_2用于和新特征组成新训练集合
         X_train_1, X_train_2, y_train_1, y_train_2 = train_test_split(X_train, y_train, test_size=0.6, random_state=999)
         self.gbdt_model.fit(X_train_1, y_train_1)
-        y_pre= self.gbdt_model.predict(X_train_2)
-        y_pro= self.gbdt_model.predict_proba(X_train_2)[:,1]
+        y_pre = self.gbdt_model.predict(X_train_2)
+        y_pro = self.gbdt_model.predict_proba(X_train_2)[:, 1]
         print("pred_leaf=T AUC Score :{0}".format(metrics.roc_auc_score(y_train_2, y_pro)))
         print("pred_leaf=T  Accuracy : {0}".format(metrics.accuracy_score(y_train_2, y_pre)))
         new_feature = self.gbdt_model.apply(X_train_2)
-        X_train_new2=self.gen_gbdt_lr_features(X_train_2,new_feature)
-        new_feature_test= self.gbdt_model.apply(X_test)
-        X_test_new=self.gen_gbdt_lr_features(X_test,new_feature_test)
+        X_train_new2 = self.gen_gbdt_lr_features(X_train_2, new_feature) if self.combine_feature else self.gen_gbdt_features(new_feature)
+        new_feature_test = self.gbdt_model.apply(X_test)
+        X_test_new = self.gen_gbdt_lr_features(X_test, new_feature_test) if self.combine_feature else self.gen_gbdt_features(new_feature_test)
         print("Training set of sample size 0.4 fewer than before")
-        return X_train_new2,y_train_2,X_test_new,y_test
-    
+        return X_train_new2, y_train_2, X_test_new, y_test
+
     ##整体训练
-    def fit_model(self,X_train,y_train,X_test,y_test):
+    def fit_model(self, X_train, y_train, X_test, y_test):
         self.gbdt_model.fit(X_train, y_train)
-        y_pre= self.gbdt_model.predict(X_test)
-        y_pro= self.gbdt_model.predict_proba(X_test)[:,1]
-        print( "pred_leaf=T  AUC Score : {0}".format(metrics.roc_auc_score(y_test, y_pro)))
+        y_pre = self.gbdt_model.predict(X_test)
+        y_pro = self.gbdt_model.predict_proba(X_test)[:, 1]
+        print("pred_leaf=T  AUC Score : {0}".format(metrics.roc_auc_score(y_test, y_pro)))
         print("pred_leaf=T  Accuracy : {0}".format(metrics.accuracy_score(y_test, y_pre)))
-        new_feature= self.gbdt_model.apply(X_train)
-        X_train_new=self.gen_gbdt_lr_features(X_train,new_feature)
-        new_feature_test= self.gbdt_model.apply(X_test)
-        X_test_new=self.gen_gbdt_lr_features(X_test,new_feature_test)
+        new_feature = self.gbdt_model.apply(X_train)
+        X_train_new = self.gen_gbdt_lr_features(X_train, new_feature) if self.combine_feature else self.gen_gbdt_features(new_feature)
+        new_feature_test = self.gbdt_model.apply(X_test)
+        X_test_new = self.gen_gbdt_lr_features(X_test, new_feature_test) if self.combine_feature else self.gen_gbdt_features(new_feature_test)
         print("Training set sample number remains the same")
-        return X_train_new,y_train,X_test_new,y_test
+        return X_train_new, y_train, X_test_new, y_test
 
     def fit(self, X, y):
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=999)
-        #generate new feature with partial data
-        X_train2,y_train2, X_test2, y_test2 = self.fit_model(X_train,y_train,X_test,y_test)
-        self.lr_model.fit(X_train2,y_train2)
-        y_pre= self.lr_model.predict(X_test2)
-        y_pro= self.lr_model.predict_proba(X_test2)[:,1]
+        # generate new feature with partial data
+        X_train2, y_train2, X_test2, y_test2 = self.fit_model(X_train, y_train, X_test, y_test)
+        self.lr_model.fit(X_train2, y_train2)
+        y_pre = self.lr_model.predict(X_test2)
+        y_pro = self.lr_model.predict_proba(X_test2)[:, 1]
         print("GBDT+LR Training AUC Score : {0}".format(metrics.roc_auc_score(y_test2, y_pro)))
         print("GBDT+LR  Training Accuracy : {0}".format(metrics.accuracy_score(y_test2, y_pre)))
         return self
-        
-    def transform(self,X):
+
+    def transform(self, X):
         new_feature_test = self.gbdt_model.apply(X)
-        X_test_new = self.gen_gbdt_lr_features(X,new_feature_test)
+        X_test_new = self.gen_gbdt_lr_features(X, new_feature_test) if self.combine_feature else self.gen_gbdt_features(new_feature_test)
         return X_test_new
-        
-    def predict(self,X):
+
+    def predict(self, X):
         test1 = self.transform(X)
         return self.lr_model.predict(test1)
-    
-    def predict_proba(self,X):
+
+    def predict_proba(self, X):
         test1 = self.transform(X)
         return self.lr_model.predict_proba(test1)
-    
 
 class LightgbmLRClassifier(BaseEstimator):
-    def __init__(self,n_estimators=30,learning_rate =0.01,max_depth=3,min_child_weight=1,gamma=0.3,subsample=0.8,colsample_bytree=0.8,objective= 'binary',nthread=-1,scale_pos_weight=1,reg_alpha=1e-05,reg_lambda=1,seed=27,lr_penalty='l2', lr_c=1.0, lr_random_state=42):
-        #gbdt model parameters
-        self.n_estimators=n_estimators
-        self.learning_rate=learning_rate
-        self.max_depth=max_depth
-        self.min_child_weight=min_child_weight
-        self.gamma=gamma
-        self.subsample=subsample
-        self.colsample_bytree=colsample_bytree
-        self.objective=objective
-        self.nthread=nthread
-        self.scale_pos_weight=scale_pos_weight
-        self.reg_alpha=reg_alpha
-        self.reg_lambda=reg_lambda
-        self.seed=seed
+    def __init__(self, combine_feature = True,n_estimators=30, learning_rate=0.3, max_depth=3, min_child_weight=1, gamma=0.3,
+                 subsample=0.7, colsample_bytree=0.7, objective='binary:logistic', nthread=-1, scale_pos_weight=1,
+                 reg_alpha=1e-05, reg_lambda=1, seed=27, lr_penalty='l2', lr_c=1.0, lr_random_state=42):
+        self.combine_feature = combine_feature
+        # gbdt model parameters
+        self.n_estimators = n_estimators
+        self.learning_rate = learning_rate
+        self.max_depth = max_depth
+        self.min_child_weight = min_child_weight
+        self.gamma = gamma
+        self.subsample = subsample
+        self.colsample_bytree = colsample_bytree
+        self.objective = objective
+        self.nthread = nthread
+        self.scale_pos_weight = scale_pos_weight
+        self.reg_alpha = reg_alpha
+        self.reg_lambda = reg_lambda
+        self.seed = seed
         print("init gbdt model:{0}".format(n_estimators))
-#        boosting_type='gbdt', num_leaves=31,  subsample_for_bin=200000,  min_split_gain=0.0,   min_child_samples=20, subsample=1.0, subsample_freq=1,   random_state=None
-        self.gbdt_model = lgb.LGBMClassifier(
-               learning_rate =self.learning_rate,
-               n_estimators=self.n_estimators,
-               max_depth=self.max_depth,
-               min_child_weight=self.min_child_weight,
-               gamma=self.gamma,
-               subsample=self.subsample,
-               colsample_bytree=self.colsample_bytree,
-               objective= self.objective,
-               nthread=self.nthread,
-               scale_pos_weight=self.scale_pos_weight,
-               reg_alpha=self.reg_alpha,
-               reg_lambda=self.reg_lambda,
-               seed=self.seed)
-        #lr model parameters
+        # self.gbdt_model = lgb.LGBMClassifier(
+        #     learning_rate=self.learning_rate,
+        #     n_estimators=self.n_estimators,
+        #     max_depth=self.max_depth,
+        #     min_child_weight=self.min_child_weight,
+        #     gamma=self.gamma,
+        #     subsample=self.subsample,
+        #     colsample_bytree=self.colsample_bytree,
+        #     objective=self.objective,
+        #     nthread=self.nthread,
+        #     scale_pos_weight=self.scale_pos_weight,
+        #     reg_alpha=self.reg_alpha,
+        #     reg_lambda=self.reg_lambda,
+        #     seed=self.seed)
+        self.gbdt_model = lgb.LGBMClassifier(boosting_type='gbdt',  max_depth=3, learning_rate=0.3, n_estimators=30, min_child_weight=1,subsample=0.7,  colsample_bytree=0.7, reg_alpha=1e-05, reg_lambda=1)
+        # lr model parameters
         self.lr_penalty = lr_penalty
         self.lr_c = lr_c
         self.lr_random_state = lr_random_state
         print("init lr model")
-        self.lr_model = LogisticRegression(C=lr_c, penalty=lr_penalty, random_state=lr_random_state)
-        #numerical feature binner
+        self.lr_model = LogisticRegression(C=lr_c, penalty=lr_penalty, tol=1e-4, solver='liblinear',
+                                           random_state=lr_random_state)
+        # numerical feature binner
+        self.one_hot_encoder = OneHotEncoder()
         self.numerical_feature_processor = None
 
     def gen_gbdt_features(self, pred_leaves, num_leaves=None):
@@ -1082,9 +1086,9 @@ class LightgbmLRClassifier(BaseEstimator):
         print("pred_leaf=T AUC Score :{0}".format(metrics.roc_auc_score(y_train_2, y_pro)))
         print("pred_leaf=T  Accuracy : {0}".format(metrics.accuracy_score(y_train_2, y_pre)))
         new_feature = self.gbdt_model.apply(X_train_2)
-        X_train_new2 = self.gen_gbdt_lr_features(X_train_2, new_feature)
+        X_train_new2 = self.gen_gbdt_lr_features(X_train_2, new_feature) if self.combine_feature else self.gen_gbdt_features(new_feature)
         new_feature_test = self.gbdt_model.apply(X_test)
-        X_test_new = self.gen_gbdt_lr_features(X_test, new_feature_test)
+        X_test_new = self.gen_gbdt_lr_features(X_test, new_feature_test) if self.combine_feature else self.gen_gbdt_features(new_feature_test)
         print("Training set of sample size 0.4 fewer than before")
         return X_train_new2, y_train_2, X_test_new, y_test
 
@@ -1096,9 +1100,9 @@ class LightgbmLRClassifier(BaseEstimator):
         print("pred_leaf=T  AUC Score : {0}".format(metrics.roc_auc_score(y_test, y_pro)))
         print("pred_leaf=T  Accuracy : {0}".format(metrics.accuracy_score(y_test, y_pre)))
         new_feature = self.gbdt_model.apply(X_train)
-        X_train_new = self.gen_gbdt_lr_features(X_train, new_feature)
+        X_train_new = self.gen_gbdt_lr_features(X_train, new_feature) if self.combine_feature else self.gen_gbdt_features(new_feature)
         new_feature_test = self.gbdt_model.apply(X_test)
-        X_test_new = self.gen_gbdt_lr_features(X_test, new_feature_test)
+        X_test_new = self.gen_gbdt_lr_features(X_test, new_feature_test) if self.combine_feature else self.gen_gbdt_features(new_feature_test)
         print("Training set sample number remains the same")
         return X_train_new, y_train, X_test_new, y_test
 
@@ -1115,7 +1119,7 @@ class LightgbmLRClassifier(BaseEstimator):
 
     def transform(self, X):
         new_feature_test = self.gbdt_model.apply(X)
-        X_test_new = self.gen_gbdt_lr_features(X, new_feature_test)
+        X_test_new = self.gen_gbdt_lr_features(X, new_feature_test) if self.combine_feature else self.gen_gbdt_features(new_feature_test)
         return X_test_new
 
     def predict(self, X):
@@ -1125,4 +1129,3 @@ class LightgbmLRClassifier(BaseEstimator):
     def predict_proba(self, X):
         test1 = self.transform(X)
         return self.lr_model.predict_proba(test1)
-    
--- model/test_gbdt_lr.py
+++ model/test_gbdt_lr.py
@@ -5,6 +5,7 @@ import lightgbm as lgb
 from sklearn.linear_model import LogisticRegression
 from sklearn.datasets  import  make_hastie_10_2
 from GBDTLRClassifier import XgboostLRClassifier
+from GBDTLRClassifier import LightgbmLRClassifier
 
 X, y = make_hastie_10_2(random_state=0)
 
@@ -15,16 +16,9 @@ lr = LogisticRegression(C=1.0, penalty='l2', random_state=42)
 lr.fit(X_train,y_train)
 y_pre= lr.predict(X_test)
 y_pro= lr.predict_proba(X_test)[:,1]
-print("LR Test AUC Score : {0}", metrics.roc_auc_score(y_test, y_pro))
-print("LR  Test Accuracy : {0}" , metrics.accuracy_score(y_test, y_pre))
-
-
-gbdtlr = XgboostLRClassifier()
-gbdtlr.fit(X_train,y_train)
-y_pre= gbdtlr.predict(X_test)
-y_pro= gbdtlr.predict_proba(X_test)[:,1]
-print("GBDT+LR Test AUC Score : {0}", metrics.roc_auc_score(y_test, y_pro))
-print("GBDT+LR  Test Accuracy : {0}" , metrics.accuracy_score(y_test, y_pre))
+print("="*60)
+print("LR Test AUC Score : {0}".format(metrics.roc_auc_score(y_test, y_pro)))
+print("LR  Test Accuracy : {0}".format(metrics.accuracy_score(y_test, y_pre)))
 
 gbm = xgb.XGBClassifier(n_estimators=30,learning_rate =0.3,max_depth=3,min_child_weight=1,gamma=0.3,subsample=0.7,colsample_bytree=0.7,objective= 'binary:logistic',nthread=-1,scale_pos_weight=1,reg_alpha=1e-05,reg_lambda=1,seed=27)
 gbm.fit(X_train,y_train)
@@ -32,33 +26,32 @@ y_pre= gbm.predict(X_test)
 # y_pre_leaf = gbm.predict(X_test,pred_leaf=True)
 # print(y_pre_leaf.shape)
 y_pro= gbm.predict_proba(X_test)[:,1]
-print("Xgboost model Test AUC Score : {0}", metrics.roc_auc_score(y_test, y_pro))
-print("Xgboost model Test Accuracy : {0}" , metrics.accuracy_score(y_test, y_pre))
+print("="*60)
+print("Xgboost model Test AUC Score: {0}".format(metrics.roc_auc_score(y_test, y_pro)))
+print("Xgboost model Test Accuracy : {0}".format(metrics.accuracy_score(y_test, y_pre)))
 
 lgbm = lgb.LGBMClassifier(boosting_type='gbdt',  max_depth=3, learning_rate=0.3, n_estimators=30, min_child_weight=1,subsample=0.7,  colsample_bytree=0.7, reg_alpha=1e-05, reg_lambda=1)
 lgbm.fit(X_train,y_train)
 y_pre= lgbm.predict(X_test)
 y_pro= lgbm.predict_proba(X_test)[:,1]
-print("lightgbm model Test AUC Score : {0}", metrics.roc_auc_score(y_test, y_pro))
-print("lightgbm model Test Accuracy : {0}" , metrics.accuracy_score(y_test, y_pre))
+print("="*60)
+print("lightgbm model Test AUC Score: {0}".format(metrics.roc_auc_score(y_test, y_pro)))
+print("lightgbm model Test Accuracy : {0}".format(metrics.accuracy_score(y_test, y_pre)))
+
+gbdtlr = XgboostLRClassifier()
+gbdtlr.fit(X_train,y_train)
+y_pre= gbdtlr.predict(X_test)
+y_pro= gbdtlr.predict_proba(X_test)[:,1]
+print("="*60)
+print("Xgboost+LR Test AUC Score : {0}".format(metrics.roc_auc_score(y_test, y_pro)))
+print("Xgboost+LR  Test Accuracy : {0}".format(metrics.accuracy_score(y_test, y_pre)))
+
+lgbmlr = LightgbmLRClassifier()
+lgbmlr.fit(X_train,y_train)
+y_pre= lgbmlr.predict(X_test)
+y_pro= lgbmlr.predict_proba(X_test)[:,1]
+print("="*60)
+print("Lightgbm+LR Test AUC Score : {0}".format(metrics.roc_auc_score(y_test, y_pro)))
+print("Lightgbm+LR  Test Accuracy : {0}".format(metrics.accuracy_score(y_test, y_pre)))
 
 
-# LR Test AUC Score : {0} 0.495476338626
-# LR  Test Accuracy : {0} 0.525
-# init gbdt model:30
-# init lr model
-# pred_leaf=T  AUC Score : 0.96882606182305
-# pred_leaf=T  Accuracy : 0.8947916666666667
-# orginfeatures:(7680, 10),predleaves:(7680, 224)
-# orginfeatures:(1920, 10),predleaves:(1920, 224)
-# Training set sample number remains the same
-# GBDT+LR Training AUC Score : 0.977231436568786
-# GBDT+LR  Training Accuracy : 0.9114583333333334
-# orginfeatures:(2400, 10),predleaves:(2400, 224)
-# orginfeatures:(2400, 10),predleaves:(2400, 224)
-# GBDT+LR Test AUC Score : {0} 0.974602495583
-# GBDT+LR  Test Accuracy : {0} 0.9075
-# Xgboost model Test AUC Score : {0} 0.96766214069
-# Xgboost model Test Accuracy : {0} 0.892083333333
-# lightgbm model Test AUC Score : {0} 0.971692046578
-# lightgbm model Test Accuracy : {0} 0.896666666667
