a2e0d0b775acc3b64b4f50961a817771bc10cc4d
tanglek <ataosky@gmail.com>
2018-01-18 14:20:01 +0800

    run

--- model/LDAModel.py
+++ model/LDAModel.py
@@ -3,7 +3,6 @@ import os
 import pickle
 import random
 from pprint import pprint
-
 import gensim
 import jieba
 import jieba.analyse
@@ -86,19 +85,12 @@ class LDA_by_sklearn():
             print(' '.join([tf_feature_names[i] for i in topic.argsort()[:-n_top_words - 1:-1]]))
             print()
 
-
-# 还未改成直接使用 cutted 文件
 class LDA_by_gensim():
-    def __init__(self, source, school):
-        self.stopwords = codecs.open(os.getcwd() + '\\data\\stopwords.txt', 'r', encoding='utf-8')
+    def __init__(self, stopwords_path = '',texts=None):
+        self.stopwords = codecs.open(stopwords_path, 'r', encoding='utf-8')
         self.stopwords = [w.strip() for w in self.stopwords]
 
-        file_path = os.getcwd() + '\\data\\raw\\' + source + '\\' + school + '.csv'
-        self.train_set = []
-        with open(file_path, 'r', encoding='utf-8') as f:
-            for line in f:
-                line = list(jieba.cut(line.strip()))
-                self.train_set.append([w for w in line if w not in self.stopwords])
+        self.train_set = texts
 
     def run_lda(self, n_topics=10):
         self.n_topics = n_topics
@@ -116,16 +108,19 @@ class ChnTfidfLDAModel():
     def __init__(self,train_texts,test_texts):
         self.train_texts = train_texts
         self.test_texts = test_texts
-        self.topicnums = [1, 5, 10, 15, 20, 30, 40]
+        self.num_topics = [5]
+        # self.num_topics = [5, 10, 15, 20, 30, 40]
         # topicnums = [1, 5, 10, 15, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+        self.no_below_this_number = 50
+        self.no_above_fraction_of_doc = 0.2
         self.ldamodels_tfidf = {}
         self.ldamodels_eval = {}
 
     # http://radimrehurek.com/topic_modeling_tutorial/2%20-%20Topic%20Modeling.html
     def intra_inter_tfidf(self,lda_model, dictionary, test_docs, num_pairs=10000):
         # Split each test document into two halves and compute topics for each half
-        part1 = [lda_model[self.tfidf[dictionary.doc2bow(tokens[:int(len(list(tokens)) / 2)])]] for tokens in test_docs]
-        part2 = [lda_model[self.tfidf[dictionary.doc2bow(tokens[int(len(list(tokens)) / 2):])]] for tokens in test_docs]
+        part1 = [lda_model[self.tfidf[dictionary.doc2bow(tokens[:int(list(tokens) / 2)])]] for tokens in test_docs]
+        part2 = [lda_model[self.tfidf[dictionary.doc2bow(tokens[int(list(tokens) / 2):])]] for tokens in test_docs]
         # Compute topic distribution similarities using cosine similarity
         # print("Average cosine similarity between corresponding parts (higher is better):")
         corresp_parts = np.mean([gensim.matutils.cossim(p1, p2) for p1, p2 in zip(part1, part2)])
@@ -167,29 +162,33 @@ class ChnTfidfLDAModel():
 
     def eval(self):
         dictionary = corpora.Dictionary(self.train_texts)
+        dictionary.filter_extremes(no_below=self.no_below_this_number, no_above=self.no_above_fraction_of_doc)
         corpus = [dictionary.doc2bow(text) for text in self.train_texts]
         self.tfidf = models.TfidfModel(corpus)
         corpus_tfidf = self.tfidf[corpus]
         print(corpus_tfidf)
-        for i in tqdm(self.topicnums, desc='num of topics'):
+
+        for i in tqdm(self.num_topics, desc='num of topics'):
             random.seed(42)
             self.ldamodels_tfidf[i] = models.ldamodel.LdaModel(corpus_tfidf, num_topics=i, id2word=dictionary)
             self.ldamodels_tfidf[i].save('./data/ldamodels_tfidf_' + str(i) + '.lda')
+            for j in range(i):
+                print('Topic {}: '.format (str(j) + self.ldamodels_tfidf[i].print_topic(j)))
 
 
-        for i in tqdm(self.topicnums, desc='num of topics'):
-            lda_model = models.ldamodel.LdaModel.load('./data/ldamodels_tfidf_' + str(i) + '.lda')
-            self.ldamodels_eval[i] = self.intra_inter_tfidf(lda_model, dictionary, self.test_texts)
-
-        pickle.dump(self.ldamodels_eval, open('./data/pub_ldamodels_tfidf_eval.pkl', 'wb'))
+        # for i in tqdm(self.topicnums, desc='num of topics'):
+        #     lda_model = models.ldamodel.LdaModel.load('./data/ldamodels_tfidf_' + str(i) + '.lda')
+        #     self.ldamodels_eval[i] = self.intra_inter_tfidf(lda_model, dictionary, self.test_texts)
+        #
+        # pickle.dump(self.ldamodels_eval, open('./data/pub_ldamodels_tfidf_eval.pkl', 'wb'))
 
     def visualize_eval(self):
         # ldamodels_eval = pickle.load(open('./data/pub_ldamodels_tfidf_eval.pkl', 'rb'))
-        corresp_parts = [self.ldamodels_eval[i][0] for i in self.topicnums]
-        random_parts = [self.ldamodels_eval[i][1] for i in self.topicnums]
+        corresp_parts = [self.ldamodels_eval[i][0] for i in self.num_topics]
+        random_parts = [self.ldamodels_eval[i][1] for i in self.num_topics]
         sns.set_context("poster")
         with sns.axes_style("darkgrid"):
-            x = self.topicnums
+            x = self.num_topics
             y1 = corresp_parts
             y2 = random_parts
             plt.plot(x, y1, label='Parts from same article')
@@ -202,10 +201,10 @@ class ChnTfidfLDAModel():
 
     def visualize_stability(self):
         lda_stability = pickle.load(open('./data/pub_lda_tfidf_stability.pkl', 'rb'))
-        mean_stability = [np.array(lda_stability[i]).mean() for i in self.topicnums[:-1]]
+        mean_stability = [np.array(lda_stability[i]).mean() for i in self.num_topics[:-1]]
 
         with sns.axes_style("darkgrid"):
-            x = self.topicnums[:-1]
+            x = self.num_topics[:-1]
             y = mean_stability
             plt.plot(x, y, label='Mean overlap')
             plt.xlim([1, 100])
@@ -222,10 +221,13 @@ class ChnTfidfLDAModel():
 
     def print_topics(self,n=10):
         """ Print the top words for each topic. """
-        lda = self.ldamodels_tfidf[0]
-        topics = lda.show_topics(topics=-1, topn=n, formatted=False)
-        for ti, topic in enumerate(topics):
-            print('topic %d: %s' % (ti, ' '.join('%s/%.2f' % (t[1], t[0]) for t in topic)))
+        for k,lda in self.ldamodels_tfidf.items():
+            topics = lda.show_topics(num_topics=k,num_words=n, log=True,formatted=False)
+            for ti, topic in enumerate(topics):
+                print('{}:{}'.format(ti,topic))
+                print('-'*60)
+            #     print('topic %d: %s' % (ti, ' '.join('%s/%.2f' % (t[1], t[0]) for t in topic)))
+            print("{}{} topics{}".format("="*25,k,"="*25))
 
 
 if __name__ == '__main__':
@@ -243,9 +245,26 @@ if __name__ == '__main__':
 
     train_texts = [texts[i] for i in train_set]
     test_texts = [texts[i] for i in test_set]
-
-
-    model = ChnTfidfLDAModel(train_texts,test_texts)
+    model = ChnTfidfLDAModel(texts,texts)
+    # model = ChnTfidfLDAModel(train_texts,test_texts)
     model.eval()
     model.print_topics()
-    # model.print_top_words()
+
+
+# if __name__ == '__main__':
+#     stop_words = [line.strip() for line in open("/home/tanglek/workspace/funlp/data/stop_words.txt", 'r', encoding='utf-8').readlines()]
+#     texts = []
+#     with codecs.open("/home/tanglek/dataset/corpus_seg_100k.txt",'r',encoding='utf-8') as f:
+#         for line in f.readlines():
+#             outstr = []
+#             for word in line.strip("\n").split(" "):
+#                 if word not in stop_words:
+#                     if word != '\t':
+#                         outstr.append(word)
+#             texts.append(outstr)
+#
+#     print("texts size:{}".format(len(texts)))
+#     # texts = wordseg.cut(texts)
+#     model = LDA_by_gensim("/home/tanglek/workspace/funlp/data/stop_words.txt",texts)
+#     model.run_lda()
+#     model.print_top_words()
